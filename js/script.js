f415e0b8ba5fa172a663ecbdfa2d732af5d8e04e
Julio Ballista <julioballista@yahoo.com>
2018-01-18 23:14:37 +0000

    Done

--- js/script.js
+++ js/script.js
@@ -8,43 +8,71 @@
 $(document).ready(function() {
 	function consonant (word) {
 		var array = word.split('');
-		var firstLetter = array[0];
+		
+		function findVowel(element){
+			return element == "a" || element == "e"|| element == "i"|| element == "o"|| element =="u"
+		} //used so that a boolean can be placed into findIndex.
+		var vowelIndex =array.findIndex(findVowel); //terminates when vowel above results to true. Prints index.
+		
+		var counter = 0;
+		var counter2 = 0;
 		
 		
-		array.splice(0,1); //Remove First Letter
-		console.log (array);
-		console.log (firstLetter);
-		array.push(firstLetter); //First letter to end.
-		console.log (array);
-		array.push("a"); //puts a at end
-		array.push("y"); // puts y at end 
-		console.log(array);
-		return array.join(""); //joins array with empty space
+		while (counter<vowelIndex){
+			while (counter <vowelIndex){
+				array.push(array[counter]);
+				counter = counter + 1;
+			}
+		} //adds beginning letters and pushes to back up until you get to the vowel.
+		
+		
+		array.reverse(); //reverse array for removal
+		
+		var x = vowelIndex;
+		while (x>0){
+			array.splice(-1,1) //removes the last item until you're done with extra letters.
+			x = x -1;
+		}
+		array.reverse(); //makes array normal again 
+		array.push("a");
+		array.push("y");
+		
+		var done = array.join("");
+		return done + " " //adds a space to deal with sentences
 	}
+	
   	function vowel (word){
-  	return word + 'yay';
-  		
-  	}
-
-
-	
-  $(".submit").click (function(){
-  	var thing= $(".input").val();
-	$(".result").text(vowel(thing));
-	
-});
-
+  	var done = word + 'yay';
+  	return done + " "; //adds a space to deal with sentences
+  	} //for vowels 
 
 // Create the wordToPigLatin function that takes a word as a parameter and returns a transfromed word. 
+function wordToPigLatin (word){
+	word = word.toLowerCase();
+	var array = word.split('');
+	var firstLetter = array[0];
+		
+	if (firstLetter === 'a' || firstLetter === 'e' || firstLetter === 'i' || firstLetter === 'o' || firstLetter === 'u'){
+		$(".result").append(vowel(word));
+	} else {
+		console.log("RUNNING CONSONANT FUNCTION");
+	    $(".result").append(consonant(word));
+	  }
+} //decides when to run consonant or foul function, prints that value to div.
 
-
-
-
-
-
-// Create the sentenceToPigLatin function that takes a sentence as a parameter
-	//Loops through all the words in the sentence and transforms each word
-	//It should return a transfromed sentance
-
-
+$('.submit').click(function(){
+	$(".result").empty();
+	var fakeinput = $(".input").val();
+	console.log(fakeinput);
+	var input = fakeinput.split(" "); //makes string into array so that it can go into master
+	console.log(input);
+	
+	var x = 0;
+	
+	while (x<(input.length)){
+		console.log(x);
+		wordToPigLatin(input[x]);
+		x = x + 1;
+	} //runs my master function for every word in the array of a sentence
+});
 });
